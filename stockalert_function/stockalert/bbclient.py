733a6108d8dbe017582abe7ec4e6bfff278b29a5
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-06-04 08:12:59 -0600

    Update bbclient.py

--- stockalert_function/stockalert/bbclient.py
+++ stockalert_function/stockalert/bbclient.py
@@ -2,34 +2,35 @@ import json
 
 import requests
 
-OUT_OF_STOCK_STATUS_LIST = [None, "NotAvailable", "ComingSoon"]
+OUT_OF_STOCK_STATUS_LIST = [None, "NotAvailable", "ComingSoon", "SoldOutOnline"]
 
 
 def is_available(item):
-    shipping = item.get("shipping", dict())
-    status = shipping.get("status")
-    purchasable = shipping.get("purchasable")  # is this useful?
+    shipping = item.get("shipping")
+    status = shipping and shipping.get("status")
+    purchasable = shipping and shipping.get("purchasable")  # is this useful?
     if status not in OUT_OF_STOCK_STATUS_LIST or purchasable:
         return True
 
     return False
 
 
+def lookup(*skus):
+    joined_skus = "|".join(skus)
+    url = f"https://www.bestbuy.ca/ecomm-api/availability/products?skus={joined_skus}"
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
+    }
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()
+
+    decoded_data = r.text.encode().decode('utf-8-sig')
+    data = json.loads(decoded_data)
+
+    return data.get("availabilities", list())
+
+
 class BestBuyClient:
-    def lookup(self, *skus):
-        joined_skus = "|".join(skus)
-        url = f"https://www.bestbuy.ca/ecomm-api/availability/products?skus={joined_skus}"
-        headers = {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
-        }
-        r = requests.get(url, headers=headers)
-        r.raise_for_status()
-
-        decoded_data = r.text.encode().decode('utf-8-sig')
-        data = json.loads(decoded_data)
-
-        return data.get("availabilities", list())
-
     def get_available_items(self, *skus):
         data = self.lookup(*skus)
         return [x for x in data if is_available(x)]
