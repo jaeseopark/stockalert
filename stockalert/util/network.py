b801bbecd3509e3da6e34f467b01c5a5bf3d7b3f
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-09-09 00:41:56 -0600

    Revert "Memex threads (#14)" (#18)
    This reverts commit a839161990d9405c4d1a618949ef809c15d05246.

--- stockalert/util/network.py
+++ stockalert/util/network.py
@@ -4,8 +4,6 @@ import requests
 
 logger = logging.getLogger(__name__)
 
-BROWSER_USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
-
 
 def get_public_ip():
     return requests.get('https://api.ipify.org', timeout=1).text
