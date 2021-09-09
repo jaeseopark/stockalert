835cecb4e9f7543c2e6047d874e6a646ff47c075
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-09-09 00:19:34 -0600

    chore: add inline doc for get_query_param

--- stockalert/util/urlstring.py
+++ stockalert/util/urlstring.py
@@ -2,6 +2,16 @@ from urllib.parse import urlparse, parse_qs
 
 
 def get_query_param(url: str, key: str) -> str:
+    """
+    Extracts the query param.
+
+    Example:
+      get_query_param("https://apple.ca?mykey=myvalue", "mykey"), will return "myvalue"
+
+    :param url: URL, including the query params
+    :param key: Name of the param
+    :return: Value of the param. Only the first will be returned if there are multiple values.
+    """
     parsed_url = urlparse(url)
     qs = parse_qs(parsed_url.query).get(key)
 
