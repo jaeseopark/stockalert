b948070bc8a691d1ca808b14521d8844857a79ea
Jaeseo Park <20038316+jaeseopark@users.noreply.github.com>
2021-09-09 22:26:24 -0600

    refactor: add parse() function (#25)

--- discord/app.py
+++ discord/app.py
@@ -1,22 +1,29 @@
 import base64
 import json
 import os
-from typing import Dict
+from typing import Dict, Tuple
 
 import boto3
 
 from client.dwclient import DiscordWebhookClient
 
 KMS_KEY = os.getenv("KMS_KEY")
+CHANNEL_MAP = dict()
 
 kmsclient = boto3.client('kms')
-
 dwclient = DiscordWebhookClient()
 
 
 def read_discord_channel_map() -> Dict[str, str]:
     with open("webhookmap.json") as fp:
         return json.load(fp)
+
+
+def parse(org_message: str) -> Tuple[str, str]:
+    channel, *rest = org_message.split(",")
+    if channel not in CHANNEL_MAP:
+        channel = "#debug"
+    return channel, ",".join(rest).strip()
 
 
 def decrypt_webhook_url(ciphertext: str) -> str:
@@ -30,13 +37,9 @@ def decrypt_webhook_url(ciphertext: str) -> str:
 
 def lambda_handler(event, context=None):
     messages = [r["Sns"]["Message"] for r in event["Records"]]
-    channel_map = read_discord_channel_map()
+    CHANNEL_MAP.update(read_discord_channel_map())
 
     for message in messages:
-        channel, *rest = message.split(",")
-        if channel not in channel_map:
-            channel = "#debug"
-        ciphertext = channel_map.get(channel)
-
-        url = decrypt_webhook_url(ciphertext)
-        dwclient.publish(url, ",".join(rest))
+        channel_name, parsed_message = parse(message)
+        url_plaintext = decrypt_webhook_url(CHANNEL_MAP.get(channel_name))
+        dwclient.publish(url_plaintext, parsed_message)
